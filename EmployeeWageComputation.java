594de09c207bac669d987e10695a5798667705bb
subzero <shubham8111994@gmail.com>
2023-02-05 12:05:54 +0530

    updated feature to calculate wage till working hours and days

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,13 +3,16 @@ public class EmployeeWageComputation {
     public static final int IS_PART_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NO_OF_WORKING_DAYS = 20;
+    public static final int NO_OF_WORKING_HOURS = 50;
 
     public static void main(String[] args) {
         //Variables
         int empHrs = 0;
-        int empWage = 0;
-        int totalWage = 0;
-        for (int i = 0; i < NO_OF_WORKING_DAYS; i++) {
+        int totalHrs = 0;
+        int totalWorkingDays = 0;
+        while (totalHrs <= NO_OF_WORKING_HOURS && totalWorkingDays < NO_OF_WORKING_DAYS) {
+            totalWorkingDays++;
+
             int empCheck = (int) Math.floor((Math.random() * 10) % 3);
             switch (empCheck) { //checking if part-time
                 case IS_PART_TIME:
@@ -21,10 +24,11 @@ public class EmployeeWageComputation {
                 default:
                     empHrs = 0;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalWage += empWage;
-            System.out.println("Emp wage: " + empWage);
+            totalHrs += empHrs;
+            System.out.println("Day#: " + totalWorkingDays + " " + "Emp hr: " + totalHrs);
         }
+        int totalWage = totalHrs * totalWorkingDays;
         System.out.println("Total employee wage: " + totalWage);
+
     }
 }
