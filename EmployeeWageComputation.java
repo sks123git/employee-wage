db9833db3657f0e18b778f0943285551d1cedf95
subzero <shubham8111994@gmail.com>
2023-02-05 11:47:32 +0530

    added feature program to calculate wage per day

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,12 +1,17 @@
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         int IS_FULL_TIME = 1;//constants
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
         double empCheck = Math.floor((Math.random() * 10) % 2);
         System.out.println(empCheck);
-        if(IS_FULL_TIME==empCheck){
-            System.out.println("Employee is full time.");
+        if(IS_FULL_TIME==empCheck){ //checking if full time or not
+            empHrs = 8;
         }else {
-            System.out.println("Employee is absent.");
+            empHrs = 0;
         }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp wage: " + empWage);
     }
 }
