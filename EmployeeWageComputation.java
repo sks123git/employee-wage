b18d4f9929c9c0fce7344a3cc956100fd5bc8061
subzero <shubham8111994@gmail.com>
2023-02-05 11:52:24 +0530

    added part time employee wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,12 +1,15 @@
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         int IS_FULL_TIME = 1;//constants
+        int IS_PART_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
-        double empCheck = Math.floor((Math.random() * 10) % 2);
+        double empCheck = Math.floor((Math.random() * 10) % 3);
         System.out.println(empCheck);
-        if(IS_FULL_TIME==empCheck){ //checking if full time or not
+        if(empCheck==IS_PART_TIME) { //checking if part time
+            empHrs = 4;
+        } else if(IS_FULL_TIME==empCheck){ //checking if full time or not
             empHrs = 8;
         }else {
             empHrs = 0;
